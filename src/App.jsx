6ff7aa3a0433098b33a0fd132e0cbfc917dc62b7
unknown <khaviyacs25@gmail.com>
2025-11-25 16:07:59 +0530

    include p tag

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ function App() {
   return (
     <>
       <h1>im kaviya</h1>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque velit perferendis minus voluptates, repellendus reiciendis hic temporibus odio, quidem deleniti ipsa quibusdam inventore, nostrum architecto blanditiis soluta accusantium quod eius.</p>
     </>
   )
 }

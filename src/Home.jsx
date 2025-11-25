d57ef13abf40b06b13dddd6bacf9a758113a3dab
unknown <khaviyacs25@gmail.com>
2025-11-25 16:12:07 +0530

    include

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,6 +5,8 @@ export default function Home() {
     <>
 
     <h1>Home</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est voluptas, odio debitis adipisci totam consectetur soluta possimus maiores quasi deserunt perferendis a autem beatae? Aliquid porro vel aliquam incidunt assumenda?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio corporis voluptas esse aliquam atque nobis voluptatem incidunt perspiciatis consequatur non in adipisci saepe voluptatum iste dolores, doloribus laudantium beatae! Ipsam!</p>
     </>
   )
 }

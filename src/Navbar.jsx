fb15f90bc3799241a774b259825d50eb81e71171
Sriranganathan-offl <sriranganathan.2002@gmail.com>
2025-11-25 20:54:31 +0530

    first commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,19 @@
+import {Link} from "react-router-dom"
+import "./index.css";
+
+const Navbar=()=>{
+    return(
+        <nav className="navbar">
+            <h2 className="logo">RoomBooking</h2>
+
+            <ul className="nav-links">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/booking">Booking</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to>Contact</Link></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;

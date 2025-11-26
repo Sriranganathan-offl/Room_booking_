1193ab34217fea6030a11e20fdef56339123a090
Sriranganathan-offl <sriranganathan.2002@gmail.com>
2025-11-26 11:27:20 +0530

    first commit

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar=()=>{
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/booking">Booking</Link></li>
                 <li><Link to="/about">About</Link></li>
-                <li><Link to>Contact</Link></li>
+                <li><Link to ="/contact">Contact</Link></li>
             </ul>
         </nav>
     );
